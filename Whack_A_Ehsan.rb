cdb30d781ed549a03e4d870d6ae5b30c7891594d
luissimo <khijbar@gmail.com>
2016-09-26 14:58:23 +0200

    game fully operational

--- Whack_A_Ehsan.rb
+++ Whack_A_Ehsan.rb
@@ -4,7 +4,7 @@ class WhackAEhsan < Gosu::Window
 
   def initialize
   	super(800, 600)
-    self.caption = 'Finish him!'
+    self.caption = 'FINISH HIM!'
     @image = Gosu::Image.new('ehsanhoofd.png')
     @hammer = Gosu::Image.new('hammer.png')
     @x = 200
@@ -15,30 +15,49 @@ class WhackAEhsan < Gosu::Window
     @velocity_y = 3 
     @visible = 0
     @hit = 0
+    @font = Gosu::Font.new(30)
+    @score = 0
+    @playing = true
+    @start_time = 0
   end
 
   def update
-  	@x += @velocity_x
-  	@y += @velocity_y
-  	@velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
-  	@velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
-  	@visible -= 1
-  	@visible = 30 if @visible < 10 && rand < 0.01
+    if @playing
+	  @x += @velocity_x
+	  @y += @velocity_y
+      @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+	  @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+	  @visible -= 1
+	  @visible = 30 if @visible < 10 && rand < 0.01
+	  @time_left = (60 - ((Gosu.milliseconds - @start_time) / 1000))
+	  @playing = false if @time_left <= 0
+    end	
   end	
 
   def button_down(id)
-  	if id == Gosu::MsLeft
-  	  if Gosu.distance(mouse_x, mouse_y, @x, @y) < 100 && @visible >= 0
-  	    @hit = 1
-  	  else 
-  	    @hit = -1
+  	if @playing 
+      if id == Gosu::MsLeft
+  	    if Gosu.distance(mouse_x, mouse_y, @x, @y) < 150 && @visible >= 0
+  	      @hit = 1
+  	      @score += 5
+  	    else 
+  	      @hit = -1
+  	      @score -= 1 if @score > 0
+  	    end
   	  end
-  	end      	
+  	else
+  	  if id == Gosu::KbSpace
+  	    @playing = true
+  	    @visible = 0
+  	    @start_time = Gosu.milliseconds
+  	    @score = 0
+  	  end  
+  	end            	
   end
 
   def draw
     @image.draw(@x - @width / 2, @y - @height / 2, 1) if @visible > 0
-    @hammer.draw(mouse_x - 130, mouse_y - 160, 1)
+    @hammer.draw(mouse_x - 220, mouse_y - 75, 1)
     if @hit == 0 
       c = Gosu::Color::NONE
     elsif @hit == 1
@@ -48,6 +67,24 @@ class WhackAEhsan < Gosu::Window
     end   
     draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c) 
     @hit = 0
+    @font.draw(@score.to_s, 700, 20, 2)
+    @font.draw(@time_left.to_s, 20, 20, 2)
+
+    unless @playing 
+      if @score < 10
+        @font.draw('Serieus, niet eens 10 punten?', 210, 100, 3)
+        @font.draw('Druk op de spatieknop om je eer te herstellen', 110, 200, 3)
+        @visible = 0
+      elsif @score == (20...50)
+        @font.draw('Mwah, kan beter...', 210, 100, 3)
+        @font.draw('Druk op de spatieknop om opnieuw te spelen', 110, 200, 3)
+        @visible = 0
+      elsif @score == (50...100)
+        @font.draw('Pit Pit Pit!!', 210, 100, 3) 
+        @font.draw('Druk op de spatieknop om nog een keer te spelen', 110, 200, 3) 
+        @visible = 0
+      end  
+    end  
   end	
 
 end
